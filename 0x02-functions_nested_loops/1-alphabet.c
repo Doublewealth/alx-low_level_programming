91456d08cdd8878b826e0142b393d20a483591a9
c033f0cd092a <doublewealth92@gmail.com>
2023-02-22 04:51:47 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter c= 'z', letter++)
+	       	_putchar(letter);
+
+	_putchar('\n');
+}
+
